80594324698ca7771e31a862e2babf1d9f2fbd70
liamg2810 <lgardn3r@gmail.com>
2025-02-11 13:36:55 +0000

    a

--- attacks/spiral/spawner.gd
+++ attacks/spiral/spawner.gd
@@ -1,26 +1,24 @@
 extends Control
 
-# [{"bullet": bullet, "velocity": velocity}]
-var bullets = []
-
-var bulletsToRemove = []
-
 var currentAngle: float = 0
 var screenSize: Vector2
+var spawnPos: Vector2 = Vector2(100, 100)
 
 func SpawnBullet():
-	var scene = load("res://bullets/templateBullet.tscn")
+	var scene = load("res://bullets/baseBullet.tscn")
 	var instance = scene.instantiate()
-	
 	add_child(instance)
 	
 	var direction = Vector2(cos(deg_to_rad(currentAngle)), sin(deg_to_rad(currentAngle)))
 	# Set velocity to have a magnitude of 10
-	var velocity = direction.normalized() * 10
+	var velocity = direction.normalized() * 5
 	
-	bullets.append({"bullet": instance, "velocity": velocity})
+	var bullet = instance.get_node("Bullet")
 	
-	currentAngle += 5
+	bullet.velocity = velocity
+	bullet.position = spawnPos
+	
+	currentAngle += 10
 	
 	await get_tree().create_timer(0.15).timeout	
 
@@ -28,26 +26,7 @@ func SpawnBullet():
 func _ready() -> void:
 	screenSize = get_viewport_rect().size
 	
-	for i in range(10000):
-		SpawnBullet()
+	for i in range(50):
 		SpawnBullet()
 		SpawnBullet()
 		await SpawnBullet()
-		
-	
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	for b in bullets:
-		var bullet = b["bullet"]
-		bullet.position += b["velocity"]
-		
-		if bullet.position > screenSize or bullet.position < Vector2.ZERO:
-			bulletsToRemove.append(b)
-			
-	for b in bulletsToRemove:
-		var bullet = b["bullet"]
-		remove_child(bullet)
-		bullets.remove_at(bullets.find(b))
-		
-	bulletsToRemove.clear()
