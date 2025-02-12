3a041e2558e36a96bf57ed5707c1f5055e3be297
liamg2810 <lgardn3r@gmail.com>
2025-02-12 15:25:29 +0000

    as

--- attacks/holeInTheWall/spawner.gd
+++ attacks/holeInTheWall/spawner.gd
@@ -5,7 +5,8 @@ var rot: int = 90
 
 const colors = ["Blue", "Red", "Green", "Purple"]
 
-var screen_width
+var screen_left: int
+var screen_width: int
 var scene = load("res://bullets/baseBullet.tscn")
 
 var rng = RandomNumberGenerator.new()
@@ -24,16 +25,16 @@ func SpawnBullet(spawnPos: Vector2, col: String, currentAngle: float):
 	bullet.rotation = deg_to_rad(currentAngle + rot)
 	bullet.velocity = velocity
 	bullet.position = spawnPos
+	bullet.screen_left = screen_left
+	bullet.screen_width = screen_width
 	
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	screenSize = get_viewport_rect().size
-	
+func start(pos: Vector2) -> void:
 	for x in range(4):
 		var currentAngle: float = 0
 		var col = colors.pick_random()
 		for i in range(45):
-			await SpawnBullet(Vector2(100, 100), col, currentAngle)
+			await SpawnBullet(pos, col, currentAngle)
 			currentAngle += 7
 		rot = rng.randf_range(25, 175)
 		await get_tree().create_timer(0.75).timeout
--- attacks/rain/spawner.gd
+++ attacks/rain/spawner.gd
@@ -1,31 +1,31 @@
 extends Control
 
-var currentAngle: float = 0
 var screenSize: Vector2
 
-var screen_width
+@export var screen_width = 100
+@export var screen_left = 0
+var rng = RandomNumberGenerator.new()
 
 func SpawnBullet(spawnPos: Vector2):
 	var scene = load("res://bullets/baseBullet.tscn")
 	var instance = scene.instantiate()
 	add_child(instance)
 	
-	var direction = Vector2(cos(deg_to_rad(currentAngle)), sin(deg_to_rad(currentAngle)))
+	var direction = Vector2(0, 1)
 	# Set velocity to have a magnitude of 10
 	var velocity = direction.normalized() * 5
 	
 	var bullet = instance.get_node("Bullet")
 	
 	bullet.velocity = velocity
-	bullet.position = spawnPos
-	
-	currentAngle += 10
+	bullet.global_position = spawnPos
+	bullet.rotation = PI / 2
+	bullet.screen_left = screen_left
+	bullet.screen_width = screen_width
 	
 	await get_tree().create_timer(0.15).timeout	
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	screenSize = get_viewport_rect().size
-	
+func start(_pos: Vector2) -> void:
 	for i in range(50):
-		await SpawnBullet(Vector2(100, 100))
+		await SpawnBullet(Vector2(rng.randi_range(screen_left, screen_left + screen_width), 0))
