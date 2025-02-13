6275e0bcbe32716295709ebd53e931b4e0316c8e
liamg2810 <lgardn3r@gmail.com>
2025-02-13 15:26:00 +0000

    not working

--- Global.gd
+++ Global.gd
@@ -5,6 +5,9 @@ signal health_changed(new_health)
 const maxHealth = 5
 var health: int = maxHealth  # Global score variable
 
+func _ready() -> void:
+	emit_signal("health_changed", health)
+
 func take_hit():
 	health = clamp(health - 1, 0, maxHealth)
 	emit_signal("health_changed", health)
