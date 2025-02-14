a4adc2c0375e5971990deb116a627e1faf8d77a0
liamg2810 <lgardn3r@gmail.com>
2025-02-14 15:03:55 +0000

    hotfix

--- Global.gd
+++ Global.gd
@@ -9,6 +9,10 @@ var health: int = maxHealth  # Global score variable
 const bossMaxHealth = 200
 var bossHealth: int = bossMaxHealth
 
+func restart():
+	health = maxHealth
+	bossHealth = bossMaxHealth
+
 func take_hit():
 	health = clamp(health - 1, 0, maxHealth)
 	emit_signal("health_changed", health)
--- startbutton.gd
+++ startbutton.gd
@@ -1,4 +1,5 @@
 extends Button
 
 func _pressed() -> void:
+	Global.restart()
 	get_tree().change_scene_to_file("res://level/level1/level1.tscn")
